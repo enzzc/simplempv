22e1dcc1ef6e7950559c734614f051f22208270f
Enzo Calamia <dev@sagebl.eu>
2016-06-14 21:57:21 +0200

    Errors handling

--- simplempv/simplempv.py
+++ simplempv/simplempv.py
@@ -4,21 +4,39 @@ import socket
 import json
 from functools import partial
 
+
+class Error(Exception):
+    pass
+
+class SocketError(Error):
+    pass
+
+class MpvError(Error):
+    pass
+
 class Mpv(object):
     commands = ['']
     def __init__(self, sockfile='/tmp/mpvsock'):
         s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        if os.path.exists(sockfile):
+        try:
             s.connect(sockfile)
-            self.fd = s
-            print(sockfile, 'loaded')
+        except OSError as e:
+            raise SocketError from e
+        self.fd = s
 
     def execute(self, command):
         data = json.dumps(command) + '\r\n'
         data = bytes(data, encoding='utf-8')
-        self.fd.send(data)
-        buf = self.fd.recv(1024)
-        return buf
+        try:
+            self.fd.send(data)
+            buf = self.fd.recv(1024)
+        except OSError as e:
+            raise SocketError from e
+        result = json.loads(buf.decode('utf-8'))
+        status = result['error']
+        if status == 'success': 
+            return result
+        raise MpvError(status)
 
     def command(self, command, *args):
         return self.execute({'command': [command, *args]})
